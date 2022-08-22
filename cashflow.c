ffb3dcad45e04d8f616cf0b67a6ee4f0547c3064
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-08-22 10:34:49 +1000

    Updated cashflow.c

--- cashflow.c
+++ cashflow.c
@@ -747,7 +747,7 @@ struct cashflow_add_child_data {
 
 int get_child_cost_callback(void * user_data, cashflow_t * cashflow, char * error){
 	int * child_cost = user_data;
-	*child_cost = cashflow->child_cost;
+	child_cost[0] = cashflow->child_cost;
 	return 1; //stop execution
 }
 
